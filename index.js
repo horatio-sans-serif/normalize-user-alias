b9231553e889d933f3df34da2fdd84442f414f8b
REDACTED <REDACTED_EMAIL>
2017-01-29 22:29:35 -0500

    Add fold-case

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const _ = require('lodash')
+const foldCase = require('fold-case')
 
 module.exports = function normalize_alias (alias) {
-  return _.kebabCase(_.toLower(_.trim(_.deburr(alias || ''))))
+  return _.kebabCase(foldCase(_.trim(_.deburr(alias || ''))))
 }
